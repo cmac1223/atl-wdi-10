7b0e6625f5c1b092ac5fbdc679c17d87fc824f3d
cmac1223 <chrismac539@gmail.com>
2017-05-16 19:26:38 -0400

    pizza exp hw

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -1,13 +1,31 @@
 // app.js
+var express = require('express'); // Loading the express module on our server
+var app = express(); // Creates a new instance of express on our server
+
+
+
 var hbs = require('hbs');
 
 app.set("view engine", "hbs");
+
 app.set('views', './views');
 
+
+
+app.get('/greeting', function(req, res) {
+    console.log(req.query);
+    
+    res.render('greeting', {
+      data: req.query.saying
+    });
+});
+
+
+
 //require express package
-var express = require('express');
+// var express = require('express');
 //save an express module as 'app'
-var app     = express();
+// var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
@@ -17,10 +35,19 @@ app.get('/topping/:type', function(req, res, next) {
 });
 
 
- app.get('/order/:amount/:size', function(req, res, next) {
+//  app.get('/order/:amount/:size', function(req, res, next) {
 
-     res.send( `Your order for ${req.params.amount} ${req.params.size}.  pizzas will be ready in 1 minute!` );
- });    
+//      res.send( `Your order for ${req.params.amount} ${req.params.size}.  pizzas will be ready in 1 minute!` );
+//  });    
+
+app.get('/order', function(req, res) {
+    console.log(req.query);
+    
+    res.render('order', {
+      data: req.query.amount,
+      data1: req.query.size
+    });
+});
 
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
