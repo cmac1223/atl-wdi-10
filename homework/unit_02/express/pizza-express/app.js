ce902ddc70a62353a5638e265c523241c95ab71e
cmac1223 <chrismac539@gmail.com>
2017-05-16 18:01:14 -0400

    "completed GET routes"

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -7,6 +7,17 @@ var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
+app.get('/topping/:type', function(req, res, next) {
+
+    res.send(`${req.params.type}.pizza! Good choice.`);
+});
+
+
+ app.get('/order/:amount/:size', function(req, res, next) {
+
+     res.send( `Your order for ${req.params.amount} ${req.params.size}.  pizzas will be ready in 1 minute!` );
+ });    
+
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
